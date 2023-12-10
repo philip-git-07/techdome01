5155cd4e6552e8953824472b5543e888960d79cd
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-12-10 15:35:59 +0000

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import "./login.scss"
-import NavBar from '../../components/navbar/NavBar'
-import { Button } from '@mui/material'
-import { useDispatch, useSelector } from 'react-redux'
-import { loginUser } from '../../actions/userAction'
-import { useNavigate } from 'react-router-dom'
-import { Bars } from 'react-loader-spinner'
-
+import React, { useEffect, useState } from 'react';
+import "./login.scss";
+import NavBar from '../../components/navbar/NavBar';
+import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../../actions/userAction';
+import { useNavigate } from 'react-router-dom';
+import { Bars } from 'react-loader-spinner';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 const Login = () => {
-
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [loader, setLoader] = useState(false);
-
     const [userData, setuserData] = useState({
         email: "",
         password: ""
@@ -24,15 +20,14 @@ const Login = () => {
 
     const handleChange = (e) => {
         setuserData({ ...userData, [e.target.name]: e.target.value });
-    }
+    };
 
-    //////////--------- for toast ---------/////////////////////
     const fun = (error) => {
         if (error) {
             toast.error(`ERROR: ${error} !`, { theme: "dark", autoClose: 2000 });
         }
         setLoader(false);
-    }
+    };
 
     const login = () => {
         if (userData.email && userData.password) {
@@ -41,47 +36,38 @@ const Login = () => {
         } else {
             toast.warning("WARNING: All fields required !", { theme: "dark", autoClose: 2000 });
         }
-    }
+    };
 
-    const { isAuthenticated } = useSelector(state => state.user)
+    const { isAuthenticated } = useSelector(state => state.user);
 
     useEffect(() => {
         const token = localStorage.getItem("userToken");
         if (token) {
             navigate("/");
         }
-    }, [isAuthenticated]);
-
-
+    }, [isAuthenticated, navigate]); // Include 'navigate' in the dependency array
 
     return (
         <>
-
-            {/* //////////////////////////////////////////////////// */}
             <ToastContainer />
-            {/* //////////////////////////////////////////////////// */}
-
-
             <div className='login'>
                 <NavBar />
-
-                {loader && <div className='loginLoader'>
-                    <Bars
-                        height="80"
-                        width="80"
-                        color="#fff"
-                        ariaLabel="bars-loading"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                        visible={true}
-                    />
-                </div>}
-
+                {loader && (
+                    <div className='loginLoader'>
+                        <Bars
+                            height="80"
+                            width="80"
+                            color="#fff"
+                            ariaLabel="bars-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                            visible={true}
+                        />
+                    </div>
+                )}
 
                 <div className='inner'>
                     <div className='signupForm'>
-
-
                         <div className='form_group'>
                             <div><Button className='but' variant='contained'>Email</Button></div>
                             <input type="email" name="email" value={userData.email} id="" placeholder='Enter your email' onChange={handleChange} />
@@ -95,13 +81,11 @@ const Login = () => {
                         <div className='btnBox'>
                             <Button variant='contained' onClick={login} className='subBt'>Login</Button>
                         </div>
-
                     </div>
                 </div>
-
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
