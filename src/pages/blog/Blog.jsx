218df9b69576b950886fc50792e8c89825d8c91e
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-12-10 15:30:11 +0000

    Update Blog.jsx

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,54 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import "./blog.scss"
-import NavBar from '../../components/navbar/NavBar'
-import img from "../../assests/img1.png"
-import BlogCard from '../../components/blogCard/BlogCard'
-
+import React, { useEffect, useState } from 'react';
+import "./blog.scss";
+import NavBar from '../../components/navbar/NavBar';
+import BlogCard from '../../components/blogCard/BlogCard';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import { useDispatch, useSelector } from 'react-redux'
-import { getBlogs } from '../../actions/blogAction'
-import { ThreeCircles } from 'react-loader-spinner'
-
+import { useDispatch, useSelector } from 'react-redux';
+import { getBlogs } from '../../actions/blogAction';
+import { ThreeCircles } from 'react-loader-spinner';
 
 const Blog = () => {
-
     const { isloading, blogs } = useSelector(state => state.blogReducer);
     const dispatch = useDispatch();
 
     const [blogArray, setBlogArray] = useState([]);
-
     const [blogData, setBlogData] = useState({
         title: "",
         description: "",
         img: ""
     });
 
-
     useEffect(() => {
         dispatch(getBlogs());
-    }, []);
+    }, [dispatch]);
 
     useEffect(() => {
-        if (blogs) {
+        if (blogs && blogs.length > 0) {
             setBlogArray(blogs);
-            setBlogData({ title: blogs[0]?.title, description: blogs[0]?.description, img: blogs[0]?.img })
+            setBlogData({
+                title: blogs[0]?.title,
+                description: blogs[0]?.description,
+                img: blogs[0]?.img
+            });
         }
     }, [blogs]);
 
-
     const setBlog = (data) => {
         setBlogData({ title: data.title, description: data.description, img: data.img })
-    }
-
-
+    };
 
     return (
-        <>
-            <div className='blogPage'>
-                <NavBar />
+        <div className='blogPage'>
+            <NavBar />
 
-                {isloading ? <div className='loader_blog'>
+            {isloading ? (
+                <div className='loader_blog'>
                     <ThreeCircles
                         height="100"
                         width="100"
@@ -61,43 +56,36 @@ const Blog = () => {
                         innerCircleColor=""
                         middleCircleColor=""
                     />
-                </div> : null}
+                </div>
+            ) : null}
 
-
-                <div className='inner'>
-                    <div className='imageCon' style={{ background: `url(${blogData.img})`, backgroundSize: "cover" }}>
-                        <div>
-                            <section> {blogData.title} </section>
-                        </div>
+            <div className='inner'>
+                <div className='imageCon' style={{ background: `url(${blogData.img})`, backgroundSize: "cover" }}>
+                    <div>
+                        <section> {blogData.title} </section>
                     </div>
-
-                    <div className='title'>Description : </div>
-
-                    <div className='Description'>
-                        {blogData.description}
-                    </div>
-
-                    <div className='bottomHeader'>
-                        Short Reads
-                        <ArrowCircleLeftIcon className='rightArrow' style={{ fontSize: "30px" }} />
-                        <ArrowCircleRightIcon className='leftArrow' style={{ fontSize: "30px" }} />
-                    </div>
-
-                    <div className='blogCardCon'>
-
-                        {blogArray.map((val, index) => {
-                            return (
-                                <BlogCard key={index} data={val} fun={setBlog} />
-                            );
-                        })}
-
-                    </div>
-
                 </div>
 
-            </div>
-        </>
-    )
-}
+                <div className='title'>Description : </div>
 
-export default Blog
+                <div className='Description'>
+                    {blogData.description}
+                </div>
+
+                <div className='bottomHeader'>
+                    Short Reads
+                    <ArrowCircleLeftIcon className='rightArrow' style={{ fontSize: "30px" }} />
+                    <ArrowCircleRightIcon className='leftArrow' style={{ fontSize: "30px" }} />
+                </div>
+
+                <div className='blogCardCon'>
+                    {blogArray.map((val, index) => (
+                        <BlogCard key={index} data={val} fun={setBlog} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
